db3efa20f30eb802e1301b279f5b7429f0b549b3
Cláudio Rico <claudiorico81@hotmail.com>
2024-07-10 23:29:14 -0300

    Cálculo de projeções criados

---
+++ src/investimentsGoals.js
@@ -0,0 +1,50 @@
+function convertToMontlyReturnRate(yearlyReturnRate) {
+  return yearlyReturnRate ** 1 / 12;
+}
+
+function generateReturnArray(
+  startingAmount = 0,
+  timeHorizon = 0,
+  timePeriod = "monthly",
+  monthlyContribution = 0,
+  returnRate = 0,
+  returnTimeFrame = "monthly"
+) {
+  if (!startingAmount || !timeHorizon) {
+    throw new Error(
+      "Investimento inicial e prazo devem ser preenchidos com valores positivos."
+    );
+  }
+
+  const finalReturnRate =
+    returnTimeFrame === "monthly"
+      ? 1 + returnRate / 100
+      : convertToMontlyReturnRate(1 + returnRate / 100);
+
+  const finalTimeHorizon =
+    timePeriod === "monthly" ? timeHorizon : timeHorizon * 12;
+
+  const referenceInvestimentObject = {
+    investedAmount: startingAmount,
+    interestReturns: 0,
+    totalInterestReturns: 0,
+    month: 0,
+    totalAmount: startingAmount,
+  };
+
+  const returnArray = [referenceInvestimentObject];
+  for (let timeReference = 1; timeReference <= finalTimeHorizon; timeReference++) {
+    const totalAmount = returnArray[timeReference - 1].totalAmount * finalReturnRate + monthlyContribution;
+    const interestReturns = returnArray[timeReference - 1].totalAmount * finalReturnRate;
+    const investedAmount = startingAmount + monthlyContribution * timeReference;
+    const totalInterestReturns = totalAmount - investedAmount;
+
+    returnArray.push({
+      investedAmount,
+      interestReturns,
+      totalInterestReturns,
+      month: timeReference,
+      totalAmount,
+    })
+  }
+}timeReference
